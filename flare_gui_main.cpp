8995f32306114a41f3248607c0362b51b8fc192a
Mark Oates <euph_boy@hotmail.com>
2015-05-01 01:57:36 -0400

    enhanced flare_gui_main.cpp

--- flare_gui_main.cpp
+++ flare_gui_main.cpp
@@ -7,10 +7,23 @@
 
 class MyProject : public FGUIScreen
 {
+private:
+	FGUIButton *button;
 public:
-	MyProject(Display *display) : FGUIScreen(display)
+	MyProject(Display *display) : FGUIScreen(display), button(NULL)
 	{
-		FGUIButton *button = new FGUIButton(this, 200, 200, 100, 60, "Hello!");
+		// create our button
+		button = new FGUIButton(this, 200, 200, 140, 70, "Click Me!");
+	}
+	void on_message(FGUIWidget *sender, std::string message) override
+	{
+		if (sender == button) // click is automatic message to parent for FGUIButton
+		{
+			// animate the button around randomly when it gets clicked
+			af::motion.cmove_to(&button->place.position.x, random_float(100, 600), 0.4);
+			af::motion.cmove_to(&button->place.position.y, random_float(100, 500), 0.4);
+			af::motion.cmove_to(&button->place.rotation, random_float(-0.4, 0.4), 0.4);
+		}
 	}
 };
 
