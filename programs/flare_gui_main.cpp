57968f14028bcab2bbcf9084ad820b078becd4cf
Mark Oates <euph_boy@hotmail.com>
2016-11-29 20:27:58 -0500

    Cleanup style format in flare_gui_main

--- programs/flare_gui_main.cpp
+++ programs/flare_gui_main.cpp
@@ -1,42 +1,43 @@
 
 
+
 #include <allegro_flare/allegro_flare.h>
 
 
 
-
-class MyProject : public UIScreen
+class MyGUIMain : public UIScreen
 {
 private:
-	UIButton *button;
+   UIButton *button;
+
 public:
-	MyProject(Display *display) : UIScreen(display), button(NULL)
-	{
-		// create our button
-		button = new UIButton(this, 200, 200, 140, 70, "Click Me!");
-	}
-	void on_message(UIWidget *sender, std::string message) override
-	{
-		if (sender == button) // click is automatic message to parent for UIButton
-		{
-			// animate the button around randomly when it gets clicked
-			Framework::motion().cmove_to(&button->place.position.x, random_float(100, 600), 0.4);
-			Framework::motion().cmove_to(&button->place.position.y, random_float(100, 500), 0.4);
-			Framework::motion().cmove_to(&button->place.rotation, random_float(-0.4, 0.4), 0.4);
-		}
-	}
+   MyGUIMain(Display *display)
+      : UIScreen(display)
+      , button(new UIButton(this, 200, 200, 140, 70, "Click Me!"))
+   {}
+
+   void on_message(UIWidget *sender, std::string message) override
+   {
+      if (sender == button) // click is automatic message to parent for UIButton
+      {
+         // animate the button around randomly when it gets clicked
+         Framework::motion().cmove_to(&button->place.position.x, random_float(100, 600), 0.4);
+         Framework::motion().cmove_to(&button->place.position.y, random_float(100, 500), 0.4);
+         Framework::motion().cmove_to(&button->place.rotation, random_float(-0.4, 0.4), 0.4);
+      }
+   }
 };
 
 
 
 int main(int argc, char **argv)
 {
-	Framework::initialize();
-	Display *display = Framework::create_display();
-	MyProject *proj = new MyProject(display);
-	Framework::run_loop();
-
-	return 0;
+   Framework::initialize();
+   Display *display = Framework::create_display();
+   MyGUIMain *proj = new MyGUIMain(display);
+   Framework::run_loop();
+   return 0;
 }
+
 
 
